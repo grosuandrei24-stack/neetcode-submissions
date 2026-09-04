140ab642dd39eb6ede63c636bef6849f86238a22
grosuandrei24-stack <grosu.andrei24@gmail.com>
2026-09-04 11:14:08 +0300

    Add: python-more-operators - submission-0

---
+++ Python For Beginners/python-more-operators/submission-0.py
@@ -0,0 +1,16 @@
+a, b, c = 2, 8, 5
+
+#Declarare variabile
+produs_a_b = a*b
+
+#Punctul 1 din problema
+print(produs_a_b//c)
+
+#Punctul 2 din problema
+print(produs_a_b%c)
+
+#Punctul 3 din problema
+print(a**b)
+
+#Punctul 4 din problema
+print(b**c)
