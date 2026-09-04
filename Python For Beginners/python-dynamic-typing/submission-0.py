efd0f206005041165b6a10d4439d0f6c8c082322
grosuandrei24-stack <grosu.andrei24@gmail.com>
2026-09-04 10:48:07 +0300

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 189.2
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "Alt string"
+print(type(variable))
+variable = [1,2]
+print(type(variable))
