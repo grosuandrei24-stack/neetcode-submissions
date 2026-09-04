ec98ae9a192498f7fb139e96495b45a5afa39dd0
grosuandrei24-stack <grosu.andrei24@gmail.com>
2026-09-04 11:09:54 +0300

    Add: python-arithmetic-operators - submission-0

---
+++ Python For Beginners/python-arithmetic-operators/submission-0.py
@@ -0,0 +1,16 @@
+a, b, c = 2, 2, 0.5
+
+#Declarare Variabile
+suma, produs = a + b + c,a * b * c
+
+#Punctul 1 din problema
+print(suma)
+
+#Punctul 2 din problema
+print(0 - suma)
+
+#Punctul 3 din problema
+print(produs)
+
+#Punctul 4 din problema
+print(suma/produs)
