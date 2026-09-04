80b5922772184c0a0437a5a7ac28b90ea4f26674
grosuandrei24-stack <grosu.andrei24@gmail.com>
2026-09-04 10:44:44 +0300

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,10 @@
+
+
+integer_type, float_type, bool_type, string_type, list_type = 10, 9.2, True, "String", [1,2,3]
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
