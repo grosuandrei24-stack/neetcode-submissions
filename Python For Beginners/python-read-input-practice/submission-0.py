9dd7834eaf8aa07e323ecfc309cc261dcd9b0569
grosuandrei24-stack <grosu.andrei24@gmail.com>
2026-09-06 23:05:08 +0300

    Add: python-read-input-practice - submission-0

---
+++ Python For Beginners/python-read-input-practice/submission-0.py
@@ -0,0 +1,12 @@
+def add_two_numbers() -> int:
+    input_user = input()
+    lista_numere = input_user.split(",")
+    return int(lista_numere[0]) + int(lista_numere[1]) 
+
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
