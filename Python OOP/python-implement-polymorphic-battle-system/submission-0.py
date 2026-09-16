8cf0e4c5ddff65d63b9c662e2e773d782cd69f98
grosuandrei24-stack <grosu.andrei24@gmail.com>
2026-09-16 17:12:44 +0300

    Add: python-implement-polymorphic-battle-system - submission-0

---
+++ Python OOP/python-implement-polymorphic-battle-system/submission-0.py
@@ -0,0 +1,36 @@
+class Hero:
+    def __init__(self, name: str, power: int):
+        self.name = name
+        self.health = 100
+        self.power = power
+    
+    def attack(self) -> int:
+        return self.power
+
+# TODO: Implement the Warrior and Mage classes
+
+class Warrior(Hero):
+    def attack(self) -> int:
+        return self.power + 10
+
+class Mage(Hero):
+
+    def __init__(self, name, power):
+        super().__init__(name,power)
+        self.health = 80
+    
+    def attack(self) -> int:
+        return self.power + 20
+# TODO: Implement the battle function
+
+def show_attack(obiect):
+    print(f"{obiect.name} attacks with {obiect.attack()} damage!")
+
+
+
+# Do not modify the following code
+warrior = Warrior("Bob", 20)
+mage = Mage("Alice", 15)
+
+show_attack(warrior)  
+show_attack(mage)    
